ede4dd2d6a684119668a008e5364f98b7c1342e1
kadzuki <heaven.roma@mail.ru>
2022-08-11 23:29:31 +0300

    Задача2

---
+++ Example1/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+
+//С исскуственным заполнением
+
+// int[] mas = new int[] {/* элементы массива */};
+// int sumEven = mas.Where(num => num % 2 == 0).Sum(); // сумма чётных
+// int sumOdd = mas.Where(num => num % 2 != 0).Sum(); // сумма нечётных
+
+//С рандомным заполнением понял так
+int[] mas = new int[5];
+for (int i = 0; i < mas.Length; i++)
+{
+          mas[i] = new Random().Next (1, 21);
+          Console.Write(mas[i] + " ");
+}
+int sumEven = mas.Where(mas => mas % 2 != 0).Sum(); // сумма нечётных
+Console.Write(sumEven);
